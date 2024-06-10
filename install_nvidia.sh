e013826fd5aba990cea596f82db473c12504f042
Romulo Gatto <romulo.gatto@gmail.com>
2024-06-10 20:13:51 -0300

    fix nvidia script

--- install_nvidia.sh
+++ install_nvidia.sh
@@ -1,5 +1,5 @@
 apk add sudo bash ca-certificates wget xz make gcc linux-headers 
-wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub 
+wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-2.27-r0.apk 
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-bin-2.27-r0.apk 
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-dev-2.27-r0.apk 
