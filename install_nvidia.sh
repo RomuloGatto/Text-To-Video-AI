e2012e41374d7f3f688d055e08ceacd2c3008706
Romulo Gatto <romulo.gatto@gmail.com>
2024-06-10 20:25:58 -0300

    adding overwrite

--- install_nvidia.sh
+++ install_nvidia.sh
@@ -5,7 +5,7 @@ wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glib
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-bin-2.27-r0.apk 
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-dev-2.27-r0.apk 
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.27-r0/glibc-i18n-2.27-r0.apk 
-apk add glibc-2.27-r0.apk glibc-bin-2.27-r0.apk glibc-dev-2.27-r0.apk glibc-i18n-2.27-r0.apk 
+apk add glibc-2.27-r0.apk glibc-bin-2.27-r0.apk glibc-dev-2.27-r0.apk glibc-i18n-2.27-r0.apk --force-overwrite 
 # apk add glibc-bin-2.27-r0.apk glibc-i18n-2.27-r0.apk glibc-2.27-r0.apk 
 /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8 
 bash NVIDIA-Linux-x86_64-390.77.run --check 
